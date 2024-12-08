0f13bb3e8de5c60cd786853da23f9e088131598e
jesuscapopons <jesuscapopons@gmail.com>
2024-12-08 16:00:12 +0000

    v1.1

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -41,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,114 +35,65 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   final _flutterReactiveBle = FlutterReactiveBle();
-  // Replace with your Arduino's service UUID
   final serviceUuid = Uuid.parse("180F");
+  String _log = "";
 
   Future<void> _requestLocationPermission() async {
-    // Request location permission
     final status = await Permission.location.request();
 
     if (status.isGranted) {
-      // Permission granted, proceed with BLE operations
       _connectToArduino();
-    } else if (status.isDenied) {
-      // Permission denied, show a dialog or snackbar
-      // explaining why the permission is needed
-      // and how to grant it in settings.
-      // You can also try requesting the permission again.
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Location Permission Required'),
-          content: const Text(
-              'This app needs location permission to scan for Bluetooth devices.'),
-          actions: [
-            TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text('OK'))
-          ],
-        ),
-      );
+    } else {
+      _log += 'Location permission denied.\n';
+      setState(() {});
     }
   }
 
   void _incrementCounter() {
     setState(() {
-      print('plus 1');
+      _log += 'plus 1\n';
       _counter++;
     });
   }
 
   void _connectToArduino() async {
     try {
-      // Start scanning for devices
-      print('Connecting... to the device');
-      final discoveredDevices = _flutterReactiveBle.scanForDevices(
-          withServices: [serviceUuid], scanMode: ScanMode.lowLatency).listen(
-        (device) {
-          // Check if the device is your Arduino Nano 33 BLE Sense Rev2
-          if (device.name == "Nano33_IMU") {
-            // Stop scanning and connect to the device
-            _flutterReactiveBle.connectToDevice(id: device.id).listen((state) {
-              if (state.connectionState == DeviceConnectionState.connected) {
-                print('Connected to the device');
-                // You can now send and receive data using the flutterReactiveBle object
-              }
-            });
-          }
-        },
-      );
+      if (await Permission.location.isGranted) {
+        _log += 'Connecting... to the device\n';
+        setState(() {});
+        final discoveredDevices = _flutterReactiveBle.scanForDevices(
+            withServices: [serviceUuid], scanMode: ScanMode.lowLatency).listen(
+          (device) {
+            if (device.name == "Nano33_IMU") {
+              _flutterReactiveBle
+                  .connectToDevice(id: device.id)
+                  .listen((state) {
+                if (state.connectionState == DeviceConnectionState.connected) {
+                  _log += 'Connected to the device\n';
+                  setState(() {});
+                }
+              });
+            }
+          },
+        );
+      } else {
+        _requestLocationPermission();
+      }
     } catch (e) {
-      print('Cannot connect, exception occured: ${e.toString()}');
-      // Handle connection errors, e.g., show a dialog
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                  title: const Text('Error'),
-                  content: const Text('Could not connect to Arduino.'),
-                  actions: [
-                    TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('OK'))
-                  ]));
+      _log += 'Cannot connect, exception occured: ${e.toString()}\n';
+      setState(() {});
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -177,9 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             ElevatedButton(
-              onPressed:
-                  _requestLocationPermission, // Call the permission request function
+              onPressed: _requestLocationPermission,
               child: const Text('Connect to Arduino'),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(_log),
+              ),
             ),
           ],
         ),
@@ -188,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
